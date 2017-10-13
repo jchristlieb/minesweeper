07b54b3cda871e579cdce19de5ce22adf8533708
Jan <mail@janchristlieb.de>
2017-10-13 12:46:45 +0200

    put code in modules board.js and game.js

--- src/board.js
+++ src/board.js
@@ -1,27 +1,7 @@
-
-class Game {
-    constructor (numberOfRows, numberOfColumns, numberOfBombs) {
-        this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs);
-    }
-
-    playMove(rowIndex, columnIndex){
-        this._board.flipTile(rowIndex, columnIndex);
-        if (this._board.playerBoard[rowIndex][columnIndex] === 'B'){
-            console.log('Game Over');
-            this._board.print();
-        } else if (!this._board.hasSafeTiles){
-            console.log('Great. You won the game.');
-        } else {
-            console.log('Current board:');
-            this._board.print();
-        }
-    }
-}
-
-class Board {
+export class Board {
     constructor (numberOfRows, numberOfColumns, numberOfBombs) {
         /*this._numberOfRows = numberOfRows;
-        this._numberOfColumns = numberOfColumns;*/
+         this._numberOfColumns = numberOfColumns;*/
         this._numberOfBombs = numberOfBombs;
         this._numberOfTiles = numberOfRows * numberOfColumns;
         this._playerBoard = Board.generatePlayerBoard(numberOfRows, numberOfColumns);
@@ -46,11 +26,11 @@ class Board {
     }
 
     getNumberOfNeighborBombs (rowIndex, columnIndex) {
-            const neighborOffsets = [ [-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
-            const numberOfRows = this._bombBoard.length;
-            const numberOfColumns = this._bombBoard[0].length;
-            let numberOfBombs = 0;
-            neighborOffsets.forEach(offset => {
+        const neighborOffsets = [ [-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
+        const numberOfRows = this._bombBoard.length;
+        const numberOfColumns = this._bombBoard[0].length;
+        let numberOfBombs = 0;
+        neighborOffsets.forEach(offset => {
             const neighborRowIndex = rowIndex + offset[0];
             const neighborColumnIndex = columnIndex + offset[1];
             if (neighborRowIndex >= 0 && neighborRowIndex <= numberOfRows && neighborColumnIndex >= 0 && neighborColumnIndex <= numberOfColumns) {
@@ -105,7 +85,4 @@ class Board {
     }
 
 }
-
-const g = new Game(3,3,1);
-g.playMove(0,1);
 
